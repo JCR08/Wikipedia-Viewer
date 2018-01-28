2cc529762045821363c40d155afed048f526bf5d
John <j.c.rowell15@gmail.com>
2018-01-27 22:35:43 -0500

    results populate page, input is now a search instead of text, removed submit button

--- script.js
+++ script.js
@@ -4,16 +4,33 @@ function getData(e){
   searchTerm = $(".searchTerm").val();
   var url = searchurl + searchTerm;
 
-  $.ajax(url, {
-    dataType: "json",
-    data:{
-      origin:'*'
-    },
-    type: "GET",
-    success: function(data) {
-      console.log(data);
+  if(searchTerm.length > 0){
+    $.ajax(url, {
+      dataType: "json",
+      data:{
+        origin:'*'
+      },
+      type: "GET",
+      success: function(data) {
+        displayResults(data);
+      }
+    });
+  } else {
+    $(".searchTerm").attr("placeholder", "Please Enter A Search Term");
+  };
+};
+
+function displayResults(data){
+  console.log(data);
+  for(var i = 0; i<data[1].length; i++){
+    for(var j = 0; j<data[2].length; j++){
+      for(var k = 0; k<data[3].length; k++){
+        if(i === j && j === k){
+          $("#searchResults").append(`<a class="resultPage" href=${data[3][k]} target="blank"><div class="result"><h3>${data[1][i]}</h3><p>${data[2][j]}</p></div></a>`);
+        }
+      }
     }
-  });
+  }
 }
 
 $("#search").submit(getData);
