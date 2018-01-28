49ab28068ceb21dff55f160f9f993c613fe80d78
John <j.c.rowell15@gmail.com>
2018-01-27 21:34:45 -0500

    getData function working with form submit on click

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-var searchurl = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&limit=20&search=';
-var searchTerm = 'trump';
-url = searchurl + searchTerm;
+function getData(e){
+  e.preventDefault();
+  var searchurl = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&limit=20&search=';
+  searchTerm = $(".searchTerm").val();
+  var url = searchurl + searchTerm;
 
-$.ajax(url, {
-      dataType: "json",
-      data:{
-        origin:'*'
-      },
-      type: "GET",
-      success: function(data) {
-        console.log(data);
-      }
-    });
+  $.ajax(url, {
+    dataType: "json",
+    data:{
+      origin:'*'
+    },
+    type: "GET",
+    success: function(data) {
+      console.log(data);
+    }
+  });
+}
+
+$("#search").submit(getData);
